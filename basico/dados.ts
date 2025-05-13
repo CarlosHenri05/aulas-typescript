db19fd3dd2154a605e3da79a87765f385fa007b8
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-05-13 11:02:04 -0300

    First commit

---
+++ basico/dados.ts
@@ -0,0 +1,23 @@
+// Tipos de dados primitivos
+
+let numero: number = 1 // Engloba double, float e int
+
+let text: string = 'Texto aqui'
+
+let trueorfalse: boolean = true
+
+// Arrays
+
+const numeros: number[] = [1, 2, 3, 4, 5]
+const textos: string[] = ['Um', 'Dois', 'Três']
+const booleans: boolean[] = [true, false, true, true]
+
+// tuplas
+
+const tudoJunto: [number, string, boolean] = [1, 'asdasd', true] // precisamos sempre seguir a ordem
+
+const numerosetextos: [number, string][] = [
+  // lista de tuplas
+  [1, 'Carlos'],
+  [2, 'Pedro'],
+]
